fff40153eb64838b6f5ece82b76aa0cdee6c807d
thanhhau <thanhhau@vn-cubesystem.com>
2022-08-23 16:16:40 +0700

    add

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    if (get_ip_address() !== get_ip_address(true))
+{
+    return view('404');
+    
+}else {
+     return view('welcome');
+    
+}
+    
+    
+});
+
+
+function get_ip_address($proxy = false)
+{
+    if ($proxy === true)
+    {
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED') as $key)
+        {
+            if (array_key_exists($key, $_SERVER) === true)
+            {
+                foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip)
+                {
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false)
+                    {
+                        return $ip;
+                    }
+                }
+            }
+        }
+    }
+
+    return $_SERVER['REMOTE_ADDR'];
+}
